6a1cd97b9126a422a337d7091a4343818fd2f8c6
Thanh <luuvanthanh121@gmail.com>
2022-03-15 16:05:51 +0700

    heroku

---
+++ database/seeders/BrandSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BrandSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'ACNE',
+            ],
+            [
+                'name' => 'ALBIRO',
+            ],
+            [
+                'name' => 'RONHILL',
+            ],
+        ];
+        DB::table('brands')->insert($data);
+    }
+}
